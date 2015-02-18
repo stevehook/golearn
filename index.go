80e6016469bec894b18d5a47fd60994efa65c043
Steve Hook <steve.hook@gmail.com>
2015-02-18 19:59:29 +0000

    Return a JSON result from our server

--- index.go
+++ index.go
@@ -4,11 +4,17 @@ import (
 	"github.com/ivpusic/neo"
 )
 
+type Person struct {
+	FirstName string
+	LastName  string
+}
+
 func main() {
 	app := neo.App()
 
 	app.Get("/", func(ctx *neo.Ctx) {
-		ctx.Res.Text("I am Neo Programmer", 200)
+		person := Person{"Bob", "Roberts"}
+		ctx.Res.Json(person, 200)
 	})
 
 	app.Start()
