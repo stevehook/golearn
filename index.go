6b301e3b30f7f417b1533a06f83bd6b8760a8964
Steve Hook <steve.hook@gmail.com>
2015-02-19 17:58:30 +0000

    Add postgre and simple endpoint to run query

--- index.go
+++ index.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/ivpusic/neo"
+	_ "github.com/lib/pq"
 )
+
+func PanicIf(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func SetupDB() *sql.DB {
+	db, err := sql.Open("postgres", "dbname=lotus_todo_development sslmode=disable")
+	PanicIf(err)
+	return db
+}
 
 type Person struct {
 	FirstName string
 	LastName  string
+}
+
+type Task struct {
+	Id        int
+	Title     string
+	Completed bool
 }
 
 func main() {
@@ -15,6 +35,21 @@ func main() {
 	app.Get("/", func(ctx *neo.Ctx) {
 		person := Person{"Bob", "Roberts"}
 		ctx.Res.Json(person, 200)
+	})
+
+	app.Get("/tasks", func(ctx *neo.Ctx) {
+		db := SetupDB()
+		rows, err := db.Query("SELECT id, title, completed FROM tasks")
+		PanicIf(err)
+		tasks := []Task{}
+		for rows.Next() {
+			task := Task{}
+			err := rows.Scan(&task.Id, &task.Title, &task.Completed)
+			PanicIf(err)
+			tasks = append(tasks, task)
+		}
+		defer rows.Close()
+		ctx.Res.Json(tasks, 200)
 	})
 
 	app.Start()
